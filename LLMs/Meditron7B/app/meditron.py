1dcca102308e25f8f841a5c00625502bd4a0966b
sandro-roth <sandro.roth@hotmail.de>
2025-07-22 15:54:00 +0200

    initial Langchain setup

---
+++ LLMs/Meditron7B/app/meditron.py
@@ -0,0 +1,12 @@
+from langchain_core.language_models import LLM
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+
+class MeditronInstanceLLM(LLM):
+    def __init__(self, model_path: str, tokenizer_path: str):
+        super.__init__()
+        object.__setattr__(self, '_model', AutoModelForCausalLM.from_pretrained(model_path))
+        object.__setattr__(self, '_tokenizer', AutoTokenizer.from_pretrained(model_path))
+
