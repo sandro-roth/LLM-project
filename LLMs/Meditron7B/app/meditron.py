58636f3693129edd8466a368fbb65b05fc5ba349
sandro-roth <sandro.roth@hotmail.de>
2025-07-23 09:14:15 +0200

    changed to property instead of __str__ method

--- LLMs/Meditron7B/app/meditron.py
+++ LLMs/Meditron7B/app/meditron.py
@@ -5,10 +5,17 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 class MeditronInstanceLLM(LLM):
-    def __init__(self, model_path: str, tokenizer_path: str):
-        super.__init__()
+    def __init__(self, model_path: str, temperature: float=0.0, max_tokens: int = 1000):
+        super().__init__()
         object.__setattr__(self, '_model', AutoModelForCausalLM.from_pretrained(model_path))
         object.__setattr__(self, '_tokenizer', AutoTokenizer.from_pretrained(model_path))
+        object.__setattr__(self, '_temperature', temperature)
+        object.__setattr__(self, '_max_tokens', max_tokens)
 
-    def __str__(self):
-        return self._model
+
+
+    @property
+    def _llm_type(self) -> str:
+        return 'meditron-7b'
+
+
