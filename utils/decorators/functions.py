d5ebda4e213411b96bc6265908be10e20ca6758d
sandro-roth <sandro.roth@hotmail.de>
2025-09-22 11:16:14 +0200

    Include logger in decorator

--- utils/decorators/functions.py
+++ utils/decorators/functions.py
@@ -1,12 +1,21 @@
 import time
+import logging
+from functools import wraps
 
 def timeit(func):
     '''Decorator to measure execution time'''
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+
+        logger = logging.getLogger(__name__)
+        logger.info(
+            f"Function {func.__qualname__} took {total_time:.4f} seconds "
+            f"with args={args} kwargs={kwargs}"
+        )
+
         return result
     return wrapper
