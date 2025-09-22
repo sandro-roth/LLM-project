30d21243a0cafc92bd2a2e734e6c77badef9e0d4
sandro-roth <sandro.roth@hotmail.de>
2025-09-22 10:42:06 +0200

    add file_hanlder

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from decorators import timeit
+from logger import setup_logging, get_logger
--- utils/logger/logger.py
+++ utils/logger/logger.py
@@ -10,3 +10,41 @@ def setup_logging(
         level: int = logging.INFO,
         to_stdout: bool = True,
 ):
+    # Main Log folder
+    base_log_dir = log_dir or os.getenv("LOG_DIR", os.path.join(os.getcwd(), "logs"))
+
+    # Each docker service gets a log folder
+    service_log_dir = os.path.join(base_log_dir, app_name)
+    os.makedirs(service_log_dir, exist_ok=True)
+
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    # Make sure Handlers are not duplicated
+    for h in list(logger.handlers):
+        logger.removeHandler(h)
+
+    log_path = os.path.join(service_log_dir, f'{datetime.now():%Y-%m-%d}.log')
+
+    file_handler = TimedRotatingFileHandler(
+        filename=log_path,
+        when="midnight",
+        interval=1,
+        backupCount=retention,
+        encoding="utf-8",
+        utc=False,  # falls du in der Container-Locale bleiben willst
+    )
+    file_fmt = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(file_fmt)
+    logger.addHandler(file_handler)
+
+    if to_stdout:
+        console = logging.StreamHandler()
+        console.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
+        logger.addHandler(console)
+
+    logger.info(f"[{app_name}] Logging initialisiert → {service_log_dir}")
+    return logger
+
+def get_logger(name:str | None = None) -> logging.Logger:
+    return logging.getLogger(name)
