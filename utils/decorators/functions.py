6602c0d36243c320907766590a801d6a49c576b1
sandro-roth <sandro.roth@hotmail.de>
2025-09-22 08:47:46 +0200

    add time measurement decorator

---
+++ utils/decorators/functions.py
@@ -0,0 +1,12 @@
+import time
+
+def timeit(func):
+    '''Decorator to measure execution time'''
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return wrapper
