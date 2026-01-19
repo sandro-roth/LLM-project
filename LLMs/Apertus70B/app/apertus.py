1ba141b3886572e2a320fef97dea6a4755602dd3
sandro-roth <sandro.roth@hotmail.de>
2026-01-19 12:49:49 +0100

    + Logger

--- LLMs/Apertus70B/app/apertus.py
+++ LLMs/Apertus70B/app/apertus.py
@@ -5,8 +5,9 @@ from pathlib import Path
 from utils import timeit
 from utils import setup_logging
 
+LOGGER = setup_logging(app_name='apertus70-inference', to_stdout=True, retention=30)
+
 class LLM_inference():
     def __init__(self):
         pass
 
-    
