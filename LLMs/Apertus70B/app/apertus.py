27f4729c5e187dc0bbb8f3a00d318ec39baf0cd1
sandro-roth <sandro.roth@hotmail.de>
2026-01-19 12:31:55 +0100

    init LLM class

---
+++ LLMs/Apertus70B/app/apertus.py
@@ -0,0 +1,12 @@
+from typing import Optional, List
+
+from pathlib import Path
+
+from utils import timeit
+from utils import setup_logging
+
+class LLM_inference():
+    def __init__(self):
+        pass
+
+    
