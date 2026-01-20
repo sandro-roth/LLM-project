38e95830a977a8f3e36821f7ef3cb99690670a14
sandro-roth <sandro.roth@hotmail.de>
2026-01-20 16:30:01 +0100

    fixed path to file

--- LLMs/Apertus70B/app/server.py
+++ LLMs/Apertus70B/app/server.py
@@ -9,7 +9,7 @@ from fastapi.responses import StreamingResponse
 from app import LLM_inference
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-model_file = Path(BASE_DIR / 'model8bit' / 'swiss-ai_Apertus-70B-Instruct-2509-Q8_0')
+model_file = Path(BASE_DIR / 'model8bit' / 'swiss-ai_Apertus-70B-Instruct-2509-Q8_0' / 'swiss-ai_Apertus-70B-Instruct-2509-Q8_0-00001-of-00002.gguf')
 
 llm = LLM_inference(
     model_path=model_file,
