fe83a5842c8eedb24bddcf0dae6bdd14aaa6c00d
sandro-roth <sandro.roth@hotmail.de>
2026-01-27 18:15:03 +0100

    include thinking flag

--- webinterface/app/streamlit.py
+++ webinterface/app/streamlit.py
@@ -288,7 +288,9 @@ class Webber:
         payload = {'prompt': text.strip(), 'system_prompt': system_message,
                    'temperature': st.session_state.get('temperature', 0.8),
                    'top_p': st.session_state.get('top_p', 0.9),
-                   'max_tokens': st.session_state.get('max_tokens', 200)}
+                   'max_tokens': st.session_state.get('max_tokens', 200),
+                   'disable_think': True if active_key == "Korrigieren" else False
+                   }
 
         # 1) Live anzeigen mit write_stream (Markdown), **ein** Platzhalter
         LOGGER.info('Livestreaming gestartet')
